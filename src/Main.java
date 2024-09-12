e2433452078e8817dc4098699cca8a4fc0f5f91f
svyataz <svyataz@gmail.com>
2024-09-12 12:39:04 +0300

    NewIdea

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+        Dictionary item_list = new Hashtable();
+        item_list.put("Amplifying_Tome", new Basic("Amplifying_Tome"));
+        System.out.println(item_list.get("Amplifying_Tome").toString());
     }
 }
